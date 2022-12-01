db82379faf518746c08558f51e67980e89c5a6f1
Jennifer Weng <jenniferweng@jennifers-mbp.lan>
2022-12-01 13:28:58 -0500

    added display query results

--- processForm.js
+++ processForm.js
@@ -19,55 +19,34 @@ app.get('/', (req, res) => {
 app.post('/process', (req, res) => {
     var query = req.body.query;
     var queryType = req.body.queryType;
-    console.log(typeof queryType);
-    queryDb(query, queryType).catch(console.dir);
-    // const processResult = queryDb(query, queryType);
-    // console.log(processResult);
-    res.send("Success!");
+    const queryObj = {[queryType]: query};
+    var MongoClient = mongodb.MongoClient;
+    MongoClient.connect(uri, (err, db) => {
+        if (err) {
+            throw err;
+        }
+        var dbo = db.db("equities");
+        dbo.collection("equities").find(queryObj).toArray((err, result) => {
+            if (err) throw err;
+            res.send(parseData(result));
+            db.close();
+        });
+    })
 })
 
-app.listen(3000);
-
-
-const client = new mongodb.MongoClient(uri);
-
-async function queryDb(query, queryType) {
-    try {
-        const db = client.db("equities");
-        const equities = db.collection("equities");
-
-        const queryObj = {[queryType]: query};
-        const resultCursor = equities.find(queryObj);
-
-        if((await resultCursor.count()) === 0) {
-            console.log("No documents found")
-        }
-
-        await resultCursor.forEach(console.dir);
-    } finally {
-        await client.close();
-    }
+function parseData(dataArr) {
+    var pdata = "";
+    dataArr.forEach((obj) => {
+        console.log(obj);
+        var company = obj.Company;
+        var ticker = obj.Ticker;
+        pdata += "<p>" 
+        pdata += company;
+        pdata += " ";
+        pdata += ticker;
+        pdata += "</p>";
+    })
+    return pdata;
 }
 
-// function queryDb(query, queryType) {
-//     const queryObj = {query: queryType};
-//     console.log("querydb is passed: ", query, " ", queryType);
-//     var data = null;
-//     mongodb.MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
-//         if (err) {
-//             throw err;
-//         }
-//         var db = client.db("equities");
-
-//         const findResult = db.collection("equities").find(queryObj);
-//         findResult.forEach(console.dir);
-
-//         // cursor.forEach((x)=> {
-//         //     console.log(x);
-//         //     data = x
-//         // });
-//         client.close();
-//         });
-//     return data;
-// }
-
+app.listen(3000);
